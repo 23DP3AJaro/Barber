ce14d9a4bdaa9b524af885b693291090ce2689b5
Artjoms <a230410aj@rvt.lv>
2025-10-07 15:23:08 +0300

    hamburger, responsive, darktheme

--- ThemeSwitch.js
+++ ThemeSwitch.js
@@ -8,4 +8,14 @@ btn.addEventListener('click', function(){
     } else {
         theme.href = 'LightTheme.css'
     }
-}) 
+})
+
+const btnBurger = document.getElementById('burger-switch-theme');
+
+btnBurger.addEventListener('click', function(){
+    if(theme.getAttribute("href") == 'LightTheme.css'){
+        theme.href = 'DarkTheme.css'
+    } else {
+        theme.href = 'LightTheme.css'
+    }
+})
